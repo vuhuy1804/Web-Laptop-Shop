47d1aefec919fc3a6624f779376e95d5d6f50f2d
Vu Huy <vuquanghuy18042005@gmail.com>
2026-08-15 10:45:21 +0700

    init

---
+++ src/test/java/com/example/WebLaptopShop/WebLaptopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebLaptopShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLaptopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
